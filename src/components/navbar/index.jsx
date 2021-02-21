19b91a63a738e620f0a7599d330fb95d49778eff
dorothyychou <dorothyychou@users.noreply.github.com>
2021-02-21 03:06:23 -0800

    added title + style update for navbar

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,15 +1,27 @@
 import './style.css';
 import {Link} from 'react-router-dom';
+import { IconContext } from "react-icons";
+import { FcIdea } from "react-icons/fc";
 
 function Navbar() {
   return (
       <div className="navbar">
-        <p className="navbar-component">
-            <Link to="/" style={{ textDecoration: 'none', color: "#F6DD90" }}>Word Problem Solver</Link>
-        </p>
-        <p className="navbar-component">
-            <Link to="/take-quiz" style={{ textDecoration: 'none', color: "#F6DD90" }}>Take Quiz</Link>
-        </p>
+        <div className="navbar-left">
+          <IconContext.Provider value={{ size: '45px',  className: "icon-style"}}>
+            <div>
+              <FcIdea />
+            </div>
+          </IconContext.Provider>
+          <text className="title">Training Arc</text>
+        </div>
+        <div className="navbar-right">
+          <p className="navbar-component-right">
+              <Link to="/input-form" style={{ textDecoration: 'none', color: "#F6DD90" }}>Word Problem Solver</Link>
+          </p>
+          <p className="navbar-component-right">
+              <Link to="/take-quiz" style={{ textDecoration: 'none', color: "#F6DD90" }}>Take Quiz</Link>
+          </p>
+        </div>
       </div>
   );
 }
