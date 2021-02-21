4b3fa7ff92f705fbc133bb258dca1a3a8f1d44c8
sq19 <shqi@ucsd.edu>
2021-02-20 20:23:05 -0800

    started routing for quiz page
    only link changes, the component does not render

--- word-problem-solver/src/App.js
+++ word-problem-solver/src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 import Navbar from './components/navbar';
 import InputForm from './components/inputform';
+import Quiz from './components/quiz';
+import {
+  Switch,
+  Route,
+  withRouter
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <InputForm />
+      <Switch>
+        <Route path="/" component={withRouter(InputForm)}></Route>
+        <Route path="/take-quiz" component={withRouter(Quiz)}></Route>
+      </Switch>
     </div>
   );
 }
--- word-problem-solver/src/components/navbar/index.jsx
+++ word-problem-solver/src/components/navbar/index.jsx
@@ -1,10 +1,16 @@
 import './style.css';
+import {Link} from 'react-router-dom';
 
 function Navbar() {
   return (
-    <div className="navbar">
-      <p className="navbar-component">Word Problem Solver</p>
-    </div>
+      <div className="navbar">
+        <p className="navbar-component">
+            <Link to="/" style={{ textDecoration: 'none', color: "#F6DD90" }}>Word Problem Solver</Link>
+        </p>
+        <p className="navbar-component">
+            <Link to="/take-quiz" style={{ textDecoration: 'none', color: "#F6DD90" }}>Take Quiz</Link>
+        </p>
+      </div>
   );
 }
 
